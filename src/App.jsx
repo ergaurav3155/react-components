3e13280e8b74223fb6ff35498b5d285e34a67c72
gaurav <139613687+ergaurav3155@users.noreply.github.com>
2024-08-24 19:19:32 +0530

    completed component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import Card from "./Components/Card";
 
 function App() {
+
   return (
     <div >
-      <Card />
+    <Card/>
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,25 +1,32 @@
 import React from 'react';
 
-function Components() {
+function Card() {
+  const data =[
+    {image: 'https://images.unsplash.com/photo-1633174524778-61a18ee54490?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFtYXpvbiUyMGJveHxlbnwwfHwwfHx8MA%3D%3D',name: "Amazon Basics", description:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis numquam nobis fuga."},
+    {image: 'https://images.unsplash.com/photo-1486572788966-cfd3df1f5b42?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2FtZXxlbnwwfHwwfHx8MA%3D%3D',name:"game", description:"Lorem ipsum, dolor sit amet consect"},
+    {image:'https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=1024x1024&w=is&k=20&c=JHHxZ25gIwgBtYyFxziTFrOuilyogTzs5KQKBhsaZhE=',name:"cooding", description:"Lorem ipsum, dolor sit amet"}
+  ]
   return (
-    <div className='w-full h-screen bg-zinc-200 flex justify-center items-center'>
+    <div className='w-full h-screen bg-zinc-200 flex justify-center items-center gap-10'>
+    {data.map((elem,index)=>(
       <div className='w-52 h-64 bg-zinc-100 rounded-md overflow-hidden'>
         <div className='w-full h-32 bg-gray-300'>
           <img
             className='w-full h-full object-cover'
-            src='https://plus.unsplash.com/premium_photo-1679168116865-7f9d1fafd328?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            src={elem.image}
             alt='Product Image'
           />
         </div>
         <div className='p-3'>
-          <h1 className='text-lg font-semibold'>Amazon Basics</h1>
+          <h1 className='text-lg font-semibold'>{elem.name}</h1>
           <p className='text-xs mt-3'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis numquam nobis fuga.
+            {elem.description}
           </p>
         </div>
       </div>
+    ))}
     </div>
   );
 }
 
-export default Components;
+export default Card;
