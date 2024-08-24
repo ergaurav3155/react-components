9dbe1cbd67c109ad14285c4a2003f5e14e1145f4
gaurav <139613687+ergaurav3155@users.noreply.github.com>
2024-08-25 05:06:35 +0530

    stste function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import Card from "./Components/Card";
-import Musicard from "./Components/Musicard";
+import React, { useState } from 'react';
 
 function App() {
+  const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
 
   return (
-    <div >
-    <Card/>
-    <Musicard/>
+    <div>
+      {val.map((item, index) => <h1 key={index}>{item}</h1>)}
+      <button 
+        onClick={() => setVal(val.filter((item, index) => index !== val.length - 1))}
+        className='px-2 py-1 bg-blue-400 rounded-full text-white text-xs'
+      >
+        click
+      </button>
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,51 +1,23 @@
 import React from 'react';
 
 function Card() {
-  const data = [
-    {
-      image: 'https://images.unsplash.com/photo-1633174524778-61a18ee54490?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFtYXpvbiUyMGJveHxlbnwwfHwwfHx8MA%3D%3D',
-      name: "Amazon Basics",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis numquam nobis fuga.",
-      inStock: true  // Corrected from inStoke to inStock
-    },
-    {
-      image: 'https://images.unsplash.com/photo-1486572788966-cfd3df1f5b42?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z2FtZXxlbnwwfHwwfHx8MA%3D%3D',
-      name: "Game",
-      description: "Lorem ipsum, dolor sit amet consect",
-      inStock: false
-    },
-    {
-      image: 'https://media.istockphoto.com/id/1224500457/photo/programming-code-abstract-technology-background-of-software-developer-and-computer-script.jpg?s=1024x1024&w=is&k=20&c=JHHxZ25gIwgBtYyFxziTFrOuilyogTzs5KQKBhsaZhE=',
-      name: "Coding",
-      description: "Lorem ipsum, dolor sit amet",
-      inStock: false
-    }
+  const data=[
+    {name:'nirvan astakama', description:"realaction between mind and sole mahadav har har mahadav"},
+    {name:'rudhar astakama', description:"realaction between mind and sole mahadav har har mahadavfewedcvef vefve edvdfv"}
   ];
 
+  const handleClickDownload=()=>{alert("chal raha hai")};
   return (
-    <div className='w-full h-screen bg-zinc-200 flex justify-center items-center gap-10'>
-      {data.map((elem, index) => (
-        <div key={index} className='w-52 h-64 bg-zinc-100 rounded-md overflow-hidden'>
-          <div className='w-full h-32 bg-gray-300'>
-            <img
-              className='w-full h-full object-cover'
-              src={elem.image}
-              alt='Product Image'
-            />
-          </div>
-          <div className='p-3'>
-            <h1 className='text-lg font-semibold'>{elem.name}</h1>
-            <p className='text-xs mt-3'>
-              {elem.description}
-            </p>
-            <button className={`px-4 py-1 ${elem.inStock ? "bg-blue-600" : "bg-red-600"} text-xs rounded text-zinc-100 mt-3`}>
-              {elem.inStock ? "In Stock" : "Out of Stock"}
-            </button>
-          </div>
-        </div>
+    <div className='w-full h-screen bg-zinc-300 flex flex-col gap-10 justify-center items-center' >
+      {data.map((item,index)=>(
+        <div className='w-90 px-3 py-2 bg-zinc-100 rounded-md'>
+        <h3 className='font-semibold text-xl'>{item.name}</h3>
+        <p className='text-xs mt-2'>{item.description}</p>
+        <button onMouseOver={handleClickDownload} className='px-2 py-1 bg-blue-400 text-xs fount-semibold text bg-zinc-100 rounded' >Download now</button>
+      </div>
       ))}
     </div>
-  );
+  )
 }
 
 export default Card;
